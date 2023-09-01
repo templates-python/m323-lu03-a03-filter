1588d0efb28c6fb3199565b8c2706998dee6ab97
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 12:09:44 +0200

    done

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-students = [
-        {'name': 'Alice', 'age': 15, 'class': '10A'},
-        {'name': 'Bob', 'age': 16, 'class': '10B'},
-        {'name': 'Charlie', 'age': 14, 'class': '9A'}
-    ]
-
 def filter_by_age():
     pass
 def filter_by_class():
@@ -13,4 +7,9 @@ def filter_students():
     pass
 
 if __name__ == '__main__':
-    pass
+    students = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'},
+        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+    ]
+    # your code here
