8506451e9a0641ac6622033661db68d7cb86bc8a
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 12:08:17 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+students = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'},
+        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+    ]
 
+def filter_by_age():
+    pass
+def filter_by_class():
+    pass
+
+def filter_students():
+    pass
 
 if __name__ == '__main__':
-    calculate()
+    pass
--- main_test.py
+++ main_test.py
@@ -1,6 +1,41 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_filter_by_age():
+    sample_students = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'},
+        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+    ]
+    result = main.filter_by_age(sample_students, 15)
+    expected = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'}
+    ]
+    assert result == expected
+
+
+def test_filter_by_class():
+    sample_students = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'},
+        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+    ]
+    result = main.filter_by_class(sample_students, '10A')
+    expected = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'}
+    ]
+    assert result == expected
+
+
+def test_filter_students():
+    sample_students = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'},
+        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+    ]
+    result = main.filter_students(sample_students, main.filter_by_age, 15)
+    expected = [
+        {'name': 'Alice', 'age': 15, 'class': '10A'},
+        {'name': 'Bob', 'age': 16, 'class': '10B'}
+    ]
+    assert result == expected
