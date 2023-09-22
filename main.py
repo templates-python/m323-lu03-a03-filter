466f1025807f0fdd9b05a0e2b5b1a47af8823ddd
graphics80 <kevin.maurizi@bzz.ch>
2023-09-22 16:24:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-def filter_by_age():
-    pass
-def filter_by_class():
-    pass
+def filter_by_age(students, age):
+    """
+    Filtert die Studentenliste nach einem gegebenen Mindestalter.
 
-def filter_students():
+    :param students: Liste von Studentendaten
+    :param age: Mindestalter für den Filter
+    :return: Gefilterte Liste von Studenten
+    """
     pass
+    
+    
+def filter_by_class(students, class_name):
+    """
+    Filtert die Studentenliste nach einer gegebenen Klasse.
+
+    :param students: Liste von Studentendaten
+    :param class_name: Klassenname für den Filter
+    :return: Gefilterte Liste von Studenten
+    """
+    pass
+    
+
+def filter_students(students, filter_function, *args):
+    """
+    Filtert die Studentenliste mit einer gegebenen Filterfunktion.
+
+    :param students: Liste von Studentendaten
+    :param filter_function: Funktion, die zum Filtern der Studenten verwendet wird
+    :param args: Zusätzliche Argumente für die Filterfunktion
+    :return: Gefilterte Liste von Studenten
+    """
+    # your code here
+    pass
+    
 
 if __name__ == '__main__':
     students = [
