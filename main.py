c009a4544c8cd1baa8d637caa1e790803b382baf
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:55 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def filter_by_age(students, min_age):
             result.append(student)
     return result
 
+
 def filter_by_class(students, class_name):
     result = []
     for student in students:
@@ -12,14 +13,16 @@ def filter_by_class(students, class_name):
             result.append(student)
     return result
 
+
 def filter_students(students, filter_function, *args):
     return filter_function(students, *args)
+
 
 if __name__ == '__main__':
     students = [
         {'name': 'Alice', 'age': 15, 'class': '10A'},
         {'name': 'Bob', 'age': 16, 'class': '10B'},
-        {'name': 'Charlie', 'age': 14, 'class': '9A'}
+        {'name': 'Charlie', 'age': 14, 'class': '9A'},
     ]
 
     filtered_by_age = filter_students(students, filter_by_age, 15)
